f01d88c47fe99ae7a5f0cd6a91738089c2db52d1
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-04-02 16:34:59 +0200

    vim_plugins, add ale norminette

--- vim_plugins.sh
+++ vim_plugins.sh
@@ -1,2 +1,3 @@
 mkdir -p ~/.vim/pack/git-plugins/start
 git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
+wget https://raw.githubusercontent.com/cassepipe/ale_norminette/main/norminette.vim -O ~/.vim/pack/git-plugins/start/ale/ale_linters/c/norminette.vim -nc
