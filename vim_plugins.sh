2bceb8f863d5eb7a797a3225f7fc52a07d53bc49
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-05-23 15:08:35 +0200

    Added vim-cpp-enhanced-highlight to vim plugins

--- vim_plugins.sh
+++ vim_plugins.sh
@@ -1,3 +1,6 @@
 mkdir -p ~/.vim/pack/git-plugins/start
 git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
 wget https://raw.githubusercontent.com/cassepipe/ale_norminette/main/norminette.vim -O ~/.vim/pack/git-plugins/start/ale/ale_linters/c/norminette.vim -nc
+
+#wget https://raw.githubusercontent.com/octol/vim-cpp-enhanced-highlight/master/after/syntax/c.vim -O ~/.vim/after/syntax/c.vim -nc
+wget https://raw.githubusercontent.com/octol/vim-cpp-enhanced-highlight/master/after/syntax/cpp.vim -O ~/.vim/after/syntax/cpp.vim -nc
