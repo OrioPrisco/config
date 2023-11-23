b41ac488cbaa1b0195189019191bf1551aa8a906
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-11-23 21:33:27 +0100

    vim_plugins.sh : make syntax directory if it does not exist

--- vim_plugins.sh
+++ vim_plugins.sh
@@ -3,4 +3,5 @@ git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-pl
 wget https://raw.githubusercontent.com/cassepipe/ale_norminette/main/norminette.vim -O ~/.vim/pack/git-plugins/start/ale/ale_linters/c/norminette.vim -nc
 
 #wget https://raw.githubusercontent.com/octol/vim-cpp-enhanced-highlight/master/after/syntax/c.vim -O ~/.vim/after/syntax/c.vim -nc
+mkdir -p ~/.vim/after/syntax/cpp.vim
 wget https://raw.githubusercontent.com/octol/vim-cpp-enhanced-highlight/master/after/syntax/cpp.vim -O ~/.vim/after/syntax/cpp.vim -nc
